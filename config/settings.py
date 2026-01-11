b244ebe2e788128942a57ef48804c2766302bfbe
Blastoche <bastien.laffargue33@gmail.com>
2026-01-11 12:06:33 +0100

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -61,13 +61,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+# TEMPLATES
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend' / 'templates',  # Ajouter cette ligne
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 """
 URL configuration for solar_simulator project.
+Configuration des URLs principales du projet.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,9 +15,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin Django
     path('admin/', admin.site.urls),
+    
+    # Frontend (site public)
+    path('', include('frontend.urls')),
+    
+    # Si tu veux une API REST plus tard
+    # path('api/', include('api.urls')),
 ]
+
+# Servir les fichiers media en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
