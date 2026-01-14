e823da157aadb29769930339d43536bb90ae6868
Blastoche <bastien.laffargue33@gmail.com>
2026-01-15 00:23:08 +0100

    Delete solar_calc/solar_calc directory

--- solar_calc/solar_calc/dataclasses/__init__.py
+++
@@ -1,5 +0,0 @@
-# Ne pas importer les dataclasses de production/consumption ici
-# Seul Django ORM doit être importé ici
-
-from .production import SolarInstallation      # dataclass de calcul
-from .consumption import ConsumptionProfile   # dataclass de calcul
